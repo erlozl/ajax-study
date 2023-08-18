25d0d2c9f5310761607ddf84fe61271002359b7b
rlozl <erlozl.0121@gmail.com>
2023-08-18 12:50:27 +0900

    ajax 최종 예제

--- src/main/java/shop/mtcoding/ajax/controller/TechController.java
+++ src/main/java/shop/mtcoding/ajax/controller/TechController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import shop.mtcoding.ajax.dto.TechResponse;
 import shop.mtcoding.ajax.model.Category;
@@ -51,6 +53,11 @@ public class TechController {
     @GetMapping("/api/category")
     public @ResponseBody List<Category> category() {
         return categoryRepository.findAll();
+    }
+
+    @GetMapping("/api/tech")
+    public @ResponseBody List<Tech> techApi(@RequestParam(defaultValue = "1") Integer categoryId) {
+        return techRepository.findByCategoryId(categoryId);
     }
 
     // ajax
